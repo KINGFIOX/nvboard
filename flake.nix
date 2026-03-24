3bfaee9d85ec33ad1dc05dc6bab86d7ede021029
wangfiox <wangfiox@gmail.com>
2026-03-24 20:58:18 +0800

    nix

--- flake.nix
+++ flake.nix
@@ -1,87 +1,55 @@
 {
   description = "NVBoard - NJU Virtual Board (SDL-based FPGA board simulation for Verilator)";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: import nixpkgs { inherit system; };
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-          # Exclude build and .git from source so Nix build is reproducible
-          src = pkgs.lib.cleanSourceWith {
-            src = pkgs.lib.cleanSource self;
-            filter = path: type:
-              type != "directory" || (builtins.baseNameOf path != "build" && builtins.baseNameOf path != ".git");
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nvboardPkg = pkgs.stdenv.mkDerivation {
+          pname = "nvboard";
+          version = "0.1.0";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
+          buildInputs = with pkgs; [ SDL2 SDL2_image SDL2_ttf ];
+
+          mesonBuildType = "release";
+
+          passthru = {
+            inherit (pkgs) SDL2 SDL2_image SDL2_ttf;
           };
-        in
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "nvboard";
-            version = "0.1.0";
-            inherit src;
+        };
+      in
+      {
+        packages = {
+          default = nvboardPkg;
+          nvboard = nvboardPkg;
+        };
 
-            nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
-            buildInputs = with pkgs; [ SDL2 SDL2_image SDL2_ttf ];
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            SDL2
+            SDL2_image
+            SDL2_ttf
+            meson
+            ninja
+            verilator
+            python3
+            pkg-config
+          ];
 
-            dontUseMesonConfigure = true;
-            mesonBuildType = "release";
-
-            configurePhase = ''
-              runHook preConfigure
-              meson setup build --prefix=$out --buildtype=release
-              runHook postConfigure
-            '';
-
-            buildPhase = ''
-              runHook preBuild
-              ninja -C build
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              runHook preInstall
-              ninja -C build install
-              runHook postInstall
-            '';
-
-            passthru = {
-              inherit (pkgs) SDL2 SDL2_image SDL2_ttf;
-            };
-          };
-
-          nvboard = self.packages.${system}.default;
-        });
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              SDL2
-              SDL2_image
-              SDL2_ttf
-              meson
-              ninja
-              verilator
-              python3
-              pkg-config
-            ];
-
-            shellHook = ''
-              export NVBOARD_HOME="$(pwd)"
-              echo "NVBOARD_HOME is set to: $NVBOARD_HOME"
-              echo "Build nvboard: meson setup build && ninja -C build"
-              echo "With upstream example (Makefile): set NVBOARD_HOME and run make -C example run"
-            '';
-          };
-        });
-    };
+          shellHook = ''
+            export NVBOARD_HOME="$(pwd)"
+            echo "NVBOARD_HOME is set to: $NVBOARD_HOME"
+            echo "Build nvboard: meson setup build && ninja -C build"
+            echo "With upstream example (Makefile): set NVBOARD_HOME and run make -C example run"
+          '';
+        };
+      }
+    );
 }
